20064ed6c40eb49988d3e17ea65fec292b3fe10a
JeongEun Heo <118269129+wjddms0501@users.noreply.github.com>
2022-12-24 12:04:40 +0900

    컴포넌트 분리

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,245 +1,133 @@
 import "./MainPage.css";
-import { SiYoutubemusic, SiYoutubegaming } from "react-icons/si";
-import { BsYoutube } from "react-icons/bs";
-import { TfiYoutube } from "react-icons/tfi";
-import { FaYoutubeSquare } from "react-icons/fa";
-import { VscGithub, VscGithubInverted } from "react-icons/vsc";
+import SideBar from "../components/SideBar/SideBar";
+import styled, { css } from "styled-components";
 
 function MainPage() {
-  const hje = "https://github.com/wjddms0501";
-  const minjae = "https://github.com/MJ-Dev92";
-  const sangjung = "https://github.com/WooLeeHappy";
-  const sunghyun = "https://github.com/kwon-sunghyun";
-  const deockbin = "https://github.com/dorothyah";
-  const suyeah = "https://github.com/addydfdf";
-
   return (
-    <div className="All">
-      <div className="guideContent">
-        <div>
-          <div className="categoryLine">
-            <div className="categoryGuide">
-              <span className="material-symbols-outlined">home</span>
-              <span>홈</span>
-            </div>
-            <div className="categoryGuide">
-              <span class="material-symbols-outlined">play_circle</span>
-              <span>Shorts</span>
-            </div>
-            <div className="categoryGuide">
-              <span className="material-symbols-outlined">subscriptions</span>
-              <span>구독</span>
-            </div>
-          </div>
-          <div className="categoryLine">
-            <div className="categoryGuide">
-              <span className="material-symbols-outlined">video_library</span>
-              <span>보관함</span>
-            </div>
-            <div className="categoryGuide">
-              <span className="material-symbols-outlined">history</span>
-              <span>시청기록</span>
-            </div>
-            <div className="categoryGuide">
-              <span className="material-symbols-outlined">smart_display</span>
-              <span>내 동영상</span>
-            </div>
-            <div className="categoryGuide">
-              <span className="material-symbols-outlined">schedule</span>
-              <span>나중에 볼 동영상</span>
-            </div>
-            <div className="categoryGuide">
-              <span className="material-symbols-outlined">thumb_up</span>
-              <span>좋아요 표시한 동영상</span>
-            </div>
-          </div>
-          <div className="categoryLine">
-            <div className="categoryGuide">
-              <p>탐색</p>
-            </div>
-            <div className="categoryGuide">
-              <span className="material-symbols-outlined">
-                local_fire_department
-              </span>
-              <span>인기 급상승</span>
-            </div>
-            <div className="categoryGuide">
-              <span className="material-symbols-outlined">music_note</span>
-              <span>음악</span>
-            </div>
-            <div className="categoryGuide">
-              <span className="material-symbols-outlined">movie</span>
-              <span>영화</span>
-            </div>
-            <div className="categoryGuide">
-              <span className="material-symbols-outlined">
-                settings_input_antenna
-              </span>
-              <span>실시간</span>
-            </div>
-            <div className="categoryGuide">
-              <span className="material-symbols-outlined">
-                stadia_controller
-              </span>
-              <span>게임</span>
-            </div>
-            <div className="categoryGuide">
-              <span className="material-symbols-outlined">military_tech</span>
-              <span>스포츠</span>
-            </div>
-            <div className="categoryGuide">
-              <span className="material-symbols-outlined">emoji_objects</span>
-              <span>학습</span>
-            </div>
-          </div>
-          <div className="categoryLine">
-            <div className="categoryGuide">
-              <p>YouTube 더보기</p>
-            </div>
-            <div className="categoryGuide">
-              <BsYoutube style={{ color: "red", marginRight: "24px" }} />
-              <span>YouTube Premium</span>
-            </div>
-            <div className="categoryGuide">
-              <TfiYoutube style={{ color: "red", marginRight: "24px" }} />
-              <span>크리에이터 스튜디오</span>
-            </div>
-            <div className="categoryGuide">
-              <SiYoutubemusic style={{ color: "red", marginRight: "24px" }} />
-              <span>YouTube Music</span>
-            </div>
-            <div className="categoryGuide">
-              <SiYoutubegaming style={{ color: "red", marginRight: "24px" }} />
-              <span>YouTube Kids</span>
-            </div>
-            <div className="categoryGuide">
-              <FaYoutubeSquare style={{ color: "red", marginRight: "24px" }} />
-              <span>YouTube TV</span>
-            </div>
-          </div>
-          <div className="categoryLine">
-            <div className="categoryGuide">
-              <span className="material-symbols-outlined">settings</span>
-              <span>설정</span>
-            </div>
-            <div className="categoryGuide">
-              <span className="material-symbols-outlined">flag</span>
-              <span>신고 기록</span>
-            </div>
-            <div className="categoryGuide">
-              <span className="material-symbols-outlined">help</span>
-              <span>고객센터</span>
-            </div>
-            <div className="categoryGuide">
-              <span className="material-symbols-outlined">sms_failed</span>
-              <span>의견 보내기</span>
-            </div>
-          </div>
-          <div className="categoryGuide">
-            <span className="material-symbols-outlined">diversity_3</span>
-            <span>C반 5조</span>
-          </div>
-          <div
-            className="categoryGuide"
-            onClick={() => {
-              window.open(minjae);
-            }}
-          >
-            <span className="material-symbols-outlined">emoji_people</span>
-            <span>
-              전민재
-              <VscGithub size="17" style={{ marginLeft: "7px" }} />
-            </span>
-          </div>
-          <div
-            className="categoryGuide"
-            onClick={() => {
-              window.open(hje);
-            }}
-          >
-            <span className="material-symbols-outlined">sledding</span>
-            <span>
-              허정은
-              <VscGithub size="17" style={{ marginLeft: "7px" }} />
-            </span>
-          </div>
-          <div
-            className="categoryGuide"
-            onClick={() => {
-              window.open(sunghyun);
-            }}
-          >
-            <span className="material-symbols-outlined">directions_run</span>
-            <span>
-              권성현
-              <VscGithubInverted size="17" style={{ marginLeft: "7px" }} />
-            </span>
-          </div>
-          <div
-            className="categoryGuide"
-            onClick={() => {
-              window.open(deockbin);
-            }}
-          >
-            <span className="material-symbols-outlined">accessibility_new</span>
-            <span>
-              홍덕빈
-              <VscGithubInverted size="17" style={{ marginLeft: "7px" }} />
-            </span>
-          </div>
-          <div
-            className="categoryGuide"
-            onClick={() => {
-              window.open(suyeah);
-            }}
-          >
-            <span className="material-symbols-outlined">sports_handball</span>
-            <span>
-              김수예
-              <VscGithubInverted size="17" style={{ marginLeft: "7px" }} />
-            </span>
-          </div>
-          <div
-            className="categoryGuide"
-            onClick={() => {
-              window.open(sangjung);
-            }}
-          >
-            <span className="material-symbols-outlined">
-              sports_martial_arts
-            </span>
-            <span>
-              이상정
-              <VscGithubInverted size="17" style={{ marginLeft: "7px" }} />
-            </span>
-          </div>
-        </div>
-      </div>
+    <StDiv All>
+      <SideBar />
       <div className="contents">
-        <div className="contentContainer">
-          <div className="contentsBox">
-            <div className="contentCard">
-              <div className="contentCardImg">정은브이로그</div>
-              <div className="contentCardChan">
-                <div className="chanImg">졍</div>
-                <div className="contentCardTxt">
-                  <span className="contentTitle">
-                    신영땅네 무작정 찾아가봄ㅋㅋ!
-                  </span>
-                  <div className="contentContents">
+        <StDiv ContentContainer>
+          <StDiv ContentsBox>
+            <StDiv ContentCard>
+              <StDiv ContentCardImg>정은브이로그</StDiv>
+              <StDiv ContentCardChan>
+                <StDiv ChanImg>졍</StDiv>
+                <StDiv ContentCardTxt>
+                  <StSpan ContentTitle>신영땅네 무작정 찾아가봄ㅋㅋ!</StSpan>
+                  <StDiv ContentContents>
                     <div>
                       <a href="채널 이동될 주소">정은땅의 슬기로운 생활</a>
                     </div>
                     <span ClassName="viewDate">조회수랑 날짜</span>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+                  </StDiv>
+                </StDiv>
+              </StDiv>
+            </StDiv>
+          </StDiv>
+        </StDiv>
       </div>
-    </div>
+    </StDiv>
   );
 }
+
+const StDiv = styled.div`
+  ${(props) =>
+    props.All &&
+    css`
+      background-color: #000000;
+      color: #ffffff;
+      display: flex;
+    `}
+
+  ${(props) =>
+    props.ContentContainer &&
+    css`
+      display: flex;
+      outline: rgb(255, 0, 0) dashed 1px;
+    `}
+
+  ${(props) =>
+    props.ContentsBox &&
+    css`
+      height: 300.487px;
+      width: 1248px;
+      max-width: 1344px;
+      min-width: auto;
+      display: flex;
+      margin: 0 16px;
+    `}
+
+  ${(props) =>
+    props.ContentCard &&
+    css`
+      gap: auto;
+      width: 312px;
+      height: 300.49px;
+      background-color: green;
+      position: relative;
+      top: 0px;
+      bottom: 0px;
+      right: 0px;
+      left: 0px;
+      cursor: pointer;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+    `}
+
+  ${(props) =>
+    props.ContentCardImg &&
+    css`
+      width: 295.99px;
+      height: 166.49px;
+      background-color: skyblue;
+      border-radius: 15px;
+    `}
+
+  ${(props) =>
+    props.ContentCardChan &&
+    css`
+      display: flex;
+      width: 295.99px;
+      height: 94px;
+      background-color: pink;
+    `}
+
+  ${(props) =>
+    props.ChanImg &&
+    css`
+      width: 36px;
+      height: 36px;
+      background-color: blue;
+      border-radius: 50%;
+      margin: 12px 12px 0 0;
+    `}
+
+  ${(props) =>
+    props.ContentCardTxt &&
+    css`
+      margin-top: 10px;
+      padding: 0 24px 0 0;
+      background-color: yellow;
+    `}
+
+  ${(props) =>
+    props.ContentContents &&
+    css`
+      a {
+        color: orange;
+      }
+      font-size: 12px;
+    `}
+`;
+
+const StSpan = styled.span`
+  ${(props) =>
+    props.ContentTitle &&
+    css`
+      font-size: 14px;
+    `}
+`;
 
 export default MainPage;
